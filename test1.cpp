6fd50aec49051a8f1eabde775f0ba5b4d3346329
escape0707 <tothesong@gmail.com>
2021-10-30 14:52:35 +0800

    Jay's version

--- test1.cpp
+++ test1.cpp
@@ -3,29 +3,19 @@
 #include <iterator>
 #include <vector>
 
-int horner(std::vector<int> list, int n, int i, int x,
-           std::vector<int> &intermediate_value);
-
 // n为多项式长度
-template <typename Container>
-int horner(std::vector<int> list, int n, int i, int x,
-           std::back_insert_iterator<Container> iter) {
-    if (i == n - 1) {
-        iter = list[i];
-        ++iter;
-        return list[i];
+template <typename Iterator>
+int horner(Iterator first, Iterator last, const int x) {
+    if (first == last) {
+        return 0;
     }
-    //用于输出每次迭代结果
-    int val = horner(list, n, i + 1, x, iter) * x + list[i];
-    iter = val;
-    ++iter;
-    return val;
+    const auto ret = horner(first + 1, last, x) * x + *first;
+    std::cout << ret << ' ';
+    return ret;
 }
 
 int main() {
-    std::vector<int> list = {1, 1, 2, 3};  // 1 + x + 2 * x ^ 2 + 3 * x ^ 3
-    std::vector<int> intermediate_value;
-    horner(list, std::size(list), 0, 2, std::back_inserter(intermediate_value));
-    copy(std::cbegin(intermediate_value), std::cend(intermediate_value),
-         std::ostream_iterator<int>(std::cout, " "));
+    std::vector<int> coefficients = {1, 1, 2,
+                                     3};  // 3 * x ^ 3 + 2 * x ^ 2 + x + 1
+    horner(std::begin(coefficients), std::end(coefficients), 2);
 }
